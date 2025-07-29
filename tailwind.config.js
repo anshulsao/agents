d7df34695cb9243e7641dc202249e37f814bbfc3
Anshul <anshul.sao@gmail.com>
2025-07-29 12:33:11 +0530

    Install and configure @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const typography = require('@tailwindcss/typography');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -125,5 +127,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
